03eb29216ba94812b2260777a09c7df3ae6efd2b
Pratik <mangal2x@gmail.com>
2023-03-06 09:01:26 +0000

    Update bookStore.py

--- bookStore.py
+++ bookStore.py
@@ -1,13 +1,16 @@
-# import tabulate
+# This program demonstrate the use of sqlite database to record the book entry. It provides user with the option to add,delete,search and update the
+# database.
 import sqlite3
 import os.path
 from tabulate import tabulate
 
 
 class BookStore:
-
+    
+    # BookStore class constructor with variable args as an argument. The args can receive one or multiple argument which are access by indexing.   
     def __init__(self, *args):
         try:
+            # Section accessed when some argument is passed to the object as table with the unique id key can only exist once.
             if len(args) > 0:
 
                 print("Creating a new database")
@@ -15,7 +18,7 @@ class BookStore:
                 self.db = sqlite3.connect('bookstore_db')
                 self.cursor = self.db.cursor()
 
-                self.cursor.execute('CREATE TABLE IF NOT EXISTS books(id INTEGER, Title TEXT, Author TEXT, Qty INTEGER)')
+                self.cursor.execute('CREATE TABLE IF NOT EXISTS books(id INTEGER PRIMARY KEY, Title TEXT, Author TEXT, Qty INTEGER)')
 
                 for row in args[0]:
                     self.start_init(row)
@@ -25,7 +28,9 @@ class BookStore:
                 self.db = sqlite3.connect('bookstore_db')
                 self.cursor = self.db.cursor()
 
+            # calling user menu
             self.user_menu()
+            # commiting all changes before exiting the program
             self.db.commit()
             print('Exiting program...')
         except Exception as e:
@@ -34,15 +39,18 @@ class BookStore:
         finally:
             self.db.close()
 
+    # Function to initialize the initial data into the database table
     def start_init(self, *detail):
         self.cursor.execute('INSERT INTO books VALUES(?, ?, ?, ?)', detail[0])
 
+    # Function to view all the data in the table of the database
     def view_all(self):
         self.cursor.execute('SELECT * FROM books')
         # for row in self.cursor:
         #     print(f'{row[0]}  {row[1]}  {row[2]}  {row[3]}')
         print(tabulate(self.cursor, headers=['id', 'Title', 'Author', 'Qty'], tablefmt='fancy_grid'))
 
+    # Function to check the book id if it exist
     def check_id(self, get_id):
         self.cursor.execute('SELECT * FROM books WHERE id = ?', (get_id,))
         if self.cursor.fetchone() is None:
@@ -50,6 +58,7 @@ class BookStore:
         else:
             return True
 
+    # Function to add new book details
     def new_entry(self):
         while True:
             try:
@@ -69,6 +78,7 @@ class BookStore:
 
         self.view_all()
 
+    # Function to update the details of the book
     def update_book(self):
         update_msg = '''
         1. Update Title
@@ -108,6 +118,7 @@ class BookStore:
             except ValueError:
                 print('Invalid Error.')
 
+    # Function to delete the book entry from the database
     def delete_book(self):
         while True:
             try:
@@ -124,6 +135,7 @@ class BookStore:
             except ValueError:
                 print('Invalid Input')
 
+    # Function to search a book
     def search_book(self):
         while True:
             try:
@@ -140,6 +152,7 @@ class BookStore:
             except ValueError:
                 print('Invalid Input')
 
+    # Function to provide main menu to the user
     def user_menu(self):
         menu = ''' 
             Enter your choice:
@@ -180,6 +193,7 @@ def main():
                     (3004, 'The Lord of the Rings', 'J.R.R Tolkein', 37),
                     (3005, 'Alice in Wonderland', 'Lewis Carroll', 12))
 
+    # Checking if the database called bookstore_db already exist and if not called Bookstore class object with argument
     if not os.path.exists('./bookstore_db'):
         BookStore(book_details)
 
